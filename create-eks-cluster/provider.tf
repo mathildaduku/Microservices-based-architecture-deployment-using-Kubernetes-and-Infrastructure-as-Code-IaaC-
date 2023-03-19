0f7f3487b48e17366a18e06fcc6ad5108c6a2e40
Duku Aghogho <mathildauku@gmail.com>
2023-03-19 15:17:03 +0100

    hi

--- create-eks-cluster/provider.tf
+++ create-eks-cluster/provider.tf
@@ -1,39 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_eks_cluster" "demo" {
-  name = "demo"
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.demo.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
-  version          = "2.16.1"
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.demo.name]
-    command     = "aws"
-  }
-}
-
 terraform {
   required_providers {
-    kubectl = {
-      source = "gavinbunney/kubectl"
-      version = "1.14.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.demo.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.demo.name]
-    command     = "aws"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
-
