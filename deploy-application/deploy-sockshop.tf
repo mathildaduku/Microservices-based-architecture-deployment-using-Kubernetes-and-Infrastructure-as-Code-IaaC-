92fa352373e38fbbd3ec8a0d08c7a95ba7cc67c1
Duku Aghogho <mathildauku@gmail.com>
2023-03-19 18:23:44 +0100

    deploy

--- deploy-application/deploy-sockshop.tf
+++ deploy-application/deploy-sockshop.tf
@@ -55,5 +55,5 @@ output "myapp_load_balancer_dns" {
 # Print out loadbalancer DNS hostname for socks deployment
 
 output "sockshop_load_balancer_dns" {
-  value = kubernetes_service.kube-voting-service.status.0.load_balancer.0.ingress.0.hostname
+  value = kubernetes_service.k8s-service-sockshop.status.0.load_balancer.0.ingress.0.hostname
 }
