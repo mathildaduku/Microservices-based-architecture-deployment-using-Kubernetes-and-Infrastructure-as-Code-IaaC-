266b2356a18204f3013be9496c17216fd0089c93
Duku Aghogho <mathildauku@gmail.com>
2023-03-19 21:25:43 +0100

    setup monitoring

--- manifests-monitoring/prometheus.tf
+++ manifests-monitoring/prometheus.tf
@@ -2,7 +2,7 @@ data "kubectl_path_documents" "manifest-files" {
     pattern = "./*.yaml"
 }
 
-resource "kubectl_manifest" "prometheus" {
+resource "kubectl_manifest" "monitoring" {
     for_each  = toset(data.kubectl_path_documents.manifest-files.documents)
     yaml_body = each.value
 }
